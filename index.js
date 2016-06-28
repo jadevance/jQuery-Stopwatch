545bfdccd5f3b4b65e72ae69cb6643d559b1fe4f
Jade Vance <jadenicolevance@gmail.com>
2016-06-28 14:17:28 -0700

    added a function to display HH:MM:SS:S to the stopwatch

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function StopWatch(callback) {
   this._delay = 10 
   this._callback = callback
 
+
   //starts the watch
   this.start = function() {
     if (this._interval) { return }
@@ -15,9 +16,15 @@ function StopWatch(callback) {
     clearInterval(this._interval)
     this._interval = null
   }
+  //reset the watch
+  this.reset = function() {
+    this._time = 0 
+    clearInterval(this._interval)
+    this._interval = null
+    return this._time
+  }
   // adds time to the counter
   this.addTime = function(){
-    console.log(this)
     this._time += this._delay
     this._callback(this._time)
   }
@@ -28,9 +35,23 @@ $(document).ready(function() {
   var container = $('#stopWatch')
   var display = container.children('.display')
   var buttons = container.children('button.stopWatch')
+  
+  function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+      , seconds = parseInt((duration/1000)%60)
+      , minutes = parseInt((duration/(1000*60))%60)
+      , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours   = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  }
+
   var stopWatch = new StopWatch(function(newTime){
     // update the display
-  display.text(newTime)    
+    display.text(msToTime(newTime))   
   })
 
   buttons.on('click', function(event) {
@@ -39,9 +60,14 @@ $(document).ready(function() {
     if (button.hasClass('start')) {
       console.log('started!')
       stopWatch.start()
-    } else {
+    } else if (button.hasClass('stop')) {
       console.log('stopped!')
       stopWatch.stop()
+    } else {
+      console.log('reset!')
+      stopWatch.reset()
+      display.text('0')
     }
   })
 })
+
